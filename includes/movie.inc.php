61551d8ca9e79a783c5eb00c14b64fa53c93800f
Manasseh Edwards <stmde03@moravian.edu>
2019-10-10 20:14:32 -0400

    Update movie.inc.php

--- includes/movie.inc.php
+++ includes/movie.inc.php
@@ -2,22 +2,16 @@
   if (isset($_POST['saveMovie'])) {
     require 'dbh.inc.php';
 	ob_start();
-
+    
     echo "<pre>", print_r($_POST), "</pre>";
     echo "<pre>", print_r($_FILES), "</pre>";
     echo "<pre>", print_r($_FILES['movieImage']), "</pre>";
-
-
-
     $movieName = $_POST['movieName'];
     $bio = $_POST['bio'];
     $bio = str_replace("'","''", $bio);
     $movieRating = $_POST['movieRating'];
     $movieImageName = time(). '_' . $_FILES['movieImage']['name'];
-
     $target = '../img/' . $movieImageName;
-
-
     if (move_uploaded_file($_FILES['movieImage']['tmp_name'], $target)) {
       echo "ok";
       $sql = "SELECT movieName FROM movies WHERE movieName=?";
@@ -46,12 +40,12 @@
             mysqli_stmt_bind_param($stmt, "ssss", $movieImageName, $bio, $movieName, $movieRating);
             mysqli_stmt_execute($stmt);
             header("Location: ../index.php?success=uploaded");
-
+            
           }
         }
       }
     }
     else {
-    header("Location: ../movieEdit.php");
+    header("Location: http://manassehedwardsportfolio-com.stackstaging.com/movieEdit.php");
   }
   }
