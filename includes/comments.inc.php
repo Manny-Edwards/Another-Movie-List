3537dc4aff3f381d652b1375a12bd57af4fc9408
Manasseh Edwards <stmde03@moravian.edu>
2019-10-10 20:15:34 -0400

    Update comments.inc.php

--- includes/comments.inc.php
+++ includes/comments.inc.php
@@ -2,6 +2,28 @@
   ob_start();
   require 'dbh.inc.php';
 
+
+   function movieAverage($conn,$movieName){
+    $sum = 0;
+    $count = 0;
+    $sql = "SELECT * FROM comments";
+    $result = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_array($result)) {
+      if ($row['movieName'] == $movieName) {
+        $sum = $sum+$row['movieRating'];
+        $count=$count+1;
+      }
+    }
+    if ($count == 0) {
+      return 0;
+    }
+    $average = $sum / $count;
+    $sql = "UPDATE movies SET movieRating='$average' WHERE movieName='$movieName'";
+    $result = mysqli_query($conn, $sql);
+
+    return intval($average);
+  }
+
   function setComments($conn){
     if (isset($_POST['commentSubmit'])) {
       $uid = $_POST['uid'];
@@ -12,6 +34,7 @@
 
       $sql = "INSERT INTO comments (uid, date, message, movieName, movieRating) VALUES ('$uid','$date','$message','$movieName', $movieRating)";
       $result = mysqli_query($conn, $sql);
+      $average = movieAverage($conn,$movieName);
     }
   }
   function getComments($conn,$movieName,$user){
@@ -66,7 +89,8 @@
 
       $sql = "UPDATE comments SET message='$message', movieRating='$movieRating' WHERE cid='$cid'";
       $result = mysqli_query($conn, $sql);
-      header("Location: ../movie.php?movieName=$movieName");
+      echo "<div class='w3-green'>This comment has been edited.</div><br>";
+      echo "<a href='movie.php?movieName=$movieName' class='w3-button w3-black' style='display:block; margin-bottom:10px;'>Go Back</a><br>";
     }
   }
   function deleteComments($conn){
@@ -75,9 +99,9 @@
       $movieName = $_POST['movieName'];
       $sql = "DELETE FROM comments WHERE cid='$cid'";
       $result = mysqli_query($conn, $sql);
-      echo "<div class='w3-red'>This comment has been deleted.</div><br>";
-      echo "<a href='movie.php?movieName=$movieName' class='w3-button w3-black' style='margin-bottom:10px;'>Ok</a><br>";
-      exit();
+      echo "<div class='w3-red'>This comment has been deleted.</div>";
+      echo "<a href='movie.php?movieName=$movieName' class='w3-button w3-black' style='display:block; margin-bottom:10px;'>Go Back</a>";
+      
     }
 
   }
